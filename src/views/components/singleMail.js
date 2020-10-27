af1d2e765e7a8307369c444fd161577405033398
hattinghruben <hattinghruben@gmail.com>
2020-10-27 19:28:19 +0200

    Group email stats Ui

--- src/views/components/singleMail.js
+++ src/views/components/singleMail.js
@@ -29,8 +29,8 @@ export default class SingleMail extends React.Component {
         return (
             <React.Fragment>
                 <Container fluid>
-                    <Row className="pt-2 pl-2"><Col sm={4}><b>{email.sender}</b></Col><Col sm={{ span: 2, offset: 6}}><a href="" className="gold-text">Resend</a></Col></Row>
-                    <Row className="pb-2 pl-2"><Col sm={true}>to&nbsp;{email.sentTo}&nbsp;&nbsp;<a href="" className="gold-text"><CaretDownFill/></a></Col></Row>
+                    <Row className="pt-2 pl-2"><Col sm={4}><b>{email.sender}</b></Col><Col sm={{ span: 2, offset: 6}}><a href="/" className="gold-text">Resend</a></Col></Row>
+                    <Row className="pb-2 pl-2"><Col sm={true}>to&nbsp;{email.sentTo}&nbsp;&nbsp;<a href="/" className="gold-text"><CaretDownFill/></a></Col></Row>
                     <Row className="p-2"><Col><ChevronRight className="dark-green"/>&nbsp;&nbsp;<CircleFill className="dark-green"/>&nbsp;&nbsp;Opens: {email.events.opens} &nbsp;Clicks: {email.events.clicks}</Col></Row>
                 </Container>
                 <Container fluid>
